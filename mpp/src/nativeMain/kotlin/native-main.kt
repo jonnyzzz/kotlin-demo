787591af97c699c6f2b2b2c2a0c4cb4f531dd3f9
Eugene Petrenko <eugene.petrenko@gmail.com>
2019-03-13 14:23:56 +0100

    disable MPP temporary

---
+++ mpp/src/nativeMain/kotlin/native-main.kt
@@ -0,0 +1,11 @@
+import kotlinx.cinterop.BooleanVar
+import kotlinx.cinterop.CPointer
+import kotlinx.cinterop.alloc
+import kotlinx.cinterop.memScoped
+import kotlinx.cinterop.ptr
+import kotlinx.cinterop.value
+
+
+fun main() {
+
+}
