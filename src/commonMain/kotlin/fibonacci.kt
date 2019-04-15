0173096e913bbec4e08061c1cd39cd256d1413a2
Eugene Petrenko <eugene.petrenko@gmail.com>
2019-04-15 19:21:05 +0200

    fibonacci task

--- src/commonMain/kotlin/fibonacci.kt
+++ src/commonMain/kotlin/fibonacci.kt
@@ -1,18 +1,8 @@
 package org.jonnyzzz.mpp
 
 
-fun fibonacci() = sequence {
-  var a_0 = 1
-  var a_1 = 1
-
-  // this sequence is infinite
-  while(true) {
-    val a_n = a_0 + a_1
-    a_0 = a_1
-    a_1 = a_n
-
-    yield(a_0)
-  }
+fun fibonacci() = sequence<Int> {
+  TODO("not implemented")
 }
 
 
