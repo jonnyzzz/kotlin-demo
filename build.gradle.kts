1e44734190c58893551d9ffc1ba2cd103aaa4cd2
Eugene Petrenko <eugene.petrenko@gmail.com>
2019-05-16 00:51:03 +1000

    fix mpp assignment (added C examples)

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,12 @@ kotlin {
 
   //TODO: use linuxX64 (on Linux) or mingwX64 (on Windows)
   macosX64("native") {
+    val main by compilations
+    main.cinterops.create("libpng") {
+      defFile = file("src/nativeMain/kotlin/lib.def")
+      packageName = "org.jonnyzzz.mpp"
+    }
+
     binaries {
       executable {
         entryPoint = "org.jonnyzzz.mpp.main"
@@ -23,19 +29,28 @@ kotlin {
   
 
   val commonMain by sourceSets.getting
+  val nativeMain by sourceSets.getting
   val javaMain by sourceSets.getting
   val jsMain by sourceSets.getting
-  
+
+  val coroutinesVersion = "1.2.1"
   commonMain.dependencies {
     implementation(kotlin("stdlib-common"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
   }
   
+  nativeMain.dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-native:$coroutinesVersion")
+  }
+
   javaMain.dependencies {
     implementation(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines:$coroutinesVersion")
   }
-  
+
   jsMain.dependencies {
     implementation(kotlin("stdlib-js"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:$coroutinesVersion")
   }
 }
 
--- src/nativeMain/kotlin/main.kt
+++ src/nativeMain/kotlin/main.kt
@@ -5,5 +5,9 @@ fun main() {
 }
 
 
-actual fun implementationName() : String = TODO("Kotlin/Native")
+actual fun implementationName(): String = TODO("Kotlin/Native")
+
+//TODO: call C function to get sttring: implementationNameFromC()?.toKStringFromUtf8()
+//TODO: see `lib.def` for the C code
+//TODO: use `returnStringFromCallback` function to get the string from C
 
