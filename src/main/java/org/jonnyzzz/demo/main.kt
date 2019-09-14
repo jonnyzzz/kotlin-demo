79a20631fd2892b2b22b92ba104c833290637b95
Eugene Petrenko <eugene.petrenko@gmail.com>
2019-09-14 17:53:15 +0200

    tune example (add args - these are used for the live coding demo)

--- src/main/java/org/jonnyzzz/demo/main.kt
+++ src/main/java/org/jonnyzzz/demo/main.kt
@@ -1,8 +1,8 @@
-@file:Suppress("PackageDirectoryMismatch")
+@file:Suppress("PackageDirectoryMismatch", "UnusedMainParameter")
 package org.jonnyzzz.demo.main
 
 
-fun main() {
+fun main(args: Array<String>) {
   println("Hello, San Francisco!")
 }
 
