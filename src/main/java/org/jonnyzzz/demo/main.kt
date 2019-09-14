9680ca614ae28ecdec3551a8cb7de840af689994
Eugene Petrenko <eugene.petrenko@gmail.com>
2019-09-14 08:35:28 +0200

    demo finals

--- src/main/java/org/jonnyzzz/demo/main.kt
+++ src/main/java/org/jonnyzzz/demo/main.kt
@@ -1,8 +1,47 @@
 @file:Suppress("PackageDirectoryMismatch")
 package org.jonnyzzz.demo.main
 
+data class User(
+        val name: String,
+        val hello: String? = null
+)
 
-fun main() {
-  println("Hello, Sydney!")
+fun readAllUsers() : List<User> = TODO()
+
+fun guessHello(name: String) : String {
+
+  return readAllUsers()
+          .filter {
+            it.name == name
+            return "sdfs"
+          }
+          .map { it.hello }
+          .firstOrNull() ?: when(name) {
+    "Space" -> "Привет"
+    "Business" -> ":("
+    else -> "Hello"
+  }
+}
+
+fun helloMessage(name: String,
+                 hello: String = guessHello(name))
+        = "$hello, ${name.capitalize()}!"
+
+fun String.hello() = helloMessage(this)
+val String.hello
+  get() = helloMessage(this)
+
+
+fun main(args: Array<String>) {
+  println(helloMessage(
+          hello = "Hello",
+          name = "Space")
+  )
+
+  for (name in args) {
+    println(
+            helloMessage(name)
+    )
+  }
 }
 
