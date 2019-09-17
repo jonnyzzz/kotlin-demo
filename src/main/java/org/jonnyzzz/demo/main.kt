f1227c9178b6d994352d313079aedeaf867fde61
Eugene Petrenko <eugene.petrenko@gmail.com>
2019-09-17 14:37:23 -0700

    works

--- src/main/java/org/jonnyzzz/demo/main.kt
+++ src/main/java/org/jonnyzzz/demo/main.kt
@@ -1,8 +1,40 @@
 @file:Suppress("PackageDirectoryMismatch", "UnusedMainParameter")
 package org.jonnyzzz.demo.main
 
+fun loadAllUsersForTheDemoNotForProduction(): List<UserInfo> = TODO()
+
+
+data class UserInfo(val name: String, val hi: String?)
+
+fun findHello(name: String) : String {
+  return loadAllUsersForTheDemoNotForProduction()
+          .filter { user -> user.name == name }
+          .mapNotNull { it.hi }
+          .firstOrNull() ?: when {
+    name.equals("Eugene", ignoreCase = true) -> "Privet"
+    name == "Oracle" -> "Good Evening!"
+    else -> "Hello"
+  }
+}
+
+
+fun hello(name: String, hi: String = findHello(name)) = """
+  $hi, 
+  ${name.capitalize()}!
+  """.trimIndent()
+
+val String.helloMessage
+  get() = helloMessage()
+
+fun String.helloMessage() = hello(this)
 
 fun main(args: Array<String>) {
-  println("Hello, San Francisco!")
+  println(
+          hello(hi = "Hi", name = "San Francisco")
+  )
+
+  for (name in args) {
+    println( name.helloMessage )
+  }
 }
 
--- src/test/java/org/jonnyzzz/demo/JavaTest.java
+++ src/test/java/org/jonnyzzz/demo/JavaTest.java
@@ -1,11 +1,12 @@
 package org.jonnyzzz.demo;
 
+import org.jonnyzzz.demo.main.MainKt;
 import org.junit.Test;
 
 public class JavaTest {
 
   @Test
   public void theTestWithSomeLongNameAndDescription() {
-
+    MainKt.hello("asdas");
   }
 }
